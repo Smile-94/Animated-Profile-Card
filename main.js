7c7b0244bfd5671991f848736d9e0384b8015b28
sazzad <mshossen75@gmail.com>
2022-11-01 17:09:30 +0600

    Initilized for upload to github

---
+++ main.js
@@ -0,0 +1,9 @@
+let userBox=document.getElementById("userBox");
+
+function openInfo(){
+    userBox.classList.remove("hide-details");
+}
+
+function closeInfo(){
+    userBox.classList.add("hide-details");
+ }
